9a79a4c07700650cb49af187450b62e8fc881097
Alex Hendershott <alex.hendershott@gmail.com>
2015-05-23 01:17:04 -0400

    Updated floating btn, show pins, hover states, and new forms

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,21 @@
+$(window).load(function(){
+   $(window).scroll(function() {
+    if ($(this).scrollTop()>185)
+     {
+        $('.btn-add-home').hide();
+        $('.btn-add-nav').show();
+     }
+    else
+     {
+      $('.btn-add-home').show();
+      $('.btn-add-nav').hide();
+      $('.imageSingle .btn-add-nav').show();
+     }
+ });
+});
+
+$(window).load(function(){
+$('#myfile').change(function(){
+		$('#path').val($(this).val());
+	});
+});
