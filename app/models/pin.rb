97a9bef9d4bb77a5a9bbdb8e41ec56431f1fc575
Alex Hendershott <alex.hendershott@gmail.com>
2015-05-24 14:37:23 -0400

    Updated styling for dropdowns and added validation

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,6 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
+  validates :device, :presence => true
   has_attached_file :image, :styles => { :medium => "2000x2000>", :thumb => "200x200>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
