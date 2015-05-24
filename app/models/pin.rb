a14da60954639f732edba8e6a674b1462460b93d
Alex Hendershott <alex.hendershott@gmail.com>
2015-05-24 15:39:10 -0400

    Added custom flash styles and more presence validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,8 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
   validates :device, :presence => true
+  validates :image, :presence => true
+  validates :description, :presence => true
   has_attached_file :image, :styles => { :medium => "2000x2000>", :thumb => "200x200>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
