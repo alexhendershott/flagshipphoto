8c0414cea6d936fb3ef6b30b6ba00f8fd9934438
Alex Hendershott <alex.hendershott@gmail.com>
2015-05-27 23:07:59 -0400

    Updated image sizes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,6 +4,6 @@ class Pin < ActiveRecord::Base
   # validates_presence_of :device, :message => "not selected"
   validates :image, :presence => true
   validates :description, :presence => true
-  has_attached_file :image, :styles => { :medium => "2000x2000>", :thumb => "200x200>" }
+  has_attached_file :image, :styles => { :large => "2280x1200", :medium => "1200x800>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
