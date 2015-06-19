97c61e32e8b714caca2690328c093f3c52a08604
Alex Hendershott <alex.hendershott@gmail.com>
2015-06-18 21:20:12 -0400

    Fixed model to set Unknown Source on blank?

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,24 +1,24 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :model, :message => "cannot be blank. Try uploading another photo."
+  validate :validate_model
   validates :image, :presence => true
   validates :description, :presence => true
   has_attached_file :image, :styles => { :large => "2280x1200", :medium => "1200x800>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-
-  # Post Process EXIF Extraction
   after_post_process  :post_process_photo
+
   def post_process_photo
     imgfile = EXIFR::JPEG.new(image.queued_for_write[:original].path)
+    self.model = imgfile.model
+    rescue EXIFR::MalformedJPEG
+  end
 
-    # Set model to EXIF data via imgfile
-    self.model         = imgfile.model
-    # # If no EXIF data set model to Unknown
-    # unless imgfile.nil? || imgfile == 0
-    #   self.model       = "Unknown Source"
-    # end
-    # rescue EXIFR::MalformedJPEG
-
+  def validate_model
+    if model.blank?
+      self.model = "Unknown Source"
+    else
+      validates_presence_of :model
+    end
   end
 
 end
