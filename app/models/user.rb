a08d1257b8fc3e553113b5e04d107ca5f1a3a074
Alex Hendershott <alex.hendershott@gmail.com>
2015-06-26 19:32:28 -0400

    Fixed current_user bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :photos
+  has_many :photos, dependent: :destroy
   after_save :mailchimp_status
 
   def mailchimp_status
